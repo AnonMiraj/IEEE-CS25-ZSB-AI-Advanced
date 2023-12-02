be51ad8c4fef4746c21b340356f7f2173aeb2db4
AnonMiraj <nabilmalek48@gmail.com>
2023-12-02 20:29:46 +0000

    first two days

---
+++ 2023/D02/src/bin/part-1.rs
@@ -0,0 +1,92 @@
+use std::fs::File;
+use std::io::{self, prelude::*, BufReader};
+
+struct Game {
+    id: u32,
+    red: u32,
+    blue: u32,
+    green: u32,
+}
+
+fn main() -> io::Result<()> {
+    let file = File::open("./input1.txt")?;
+    let reader = BufReader::new(file);
+    let mut game = Game {
+        id: 1,
+        red: 0,
+        blue: 0,
+        green: 0,
+    };
+    let mut sum: u32 = 0;
+    for line_res in reader.lines() {
+        let line = line_res?;
+        let mut parts = line.splitn(2, ':');
+        if let Some(second_part) = parts.nth(1) {
+            println!("{}", second_part);
+            let parts: Vec<&str> = second_part.split(';').collect();
+
+            parts.iter().enumerate().for_each(|(index, part)| {
+                if let Some(index) = part.find("red") {
+                    let st = &part[index - 3..index];
+                    let mst = &st.replace(" ", "");
+
+                    match mst.parse::<u32>() {
+                        Ok(result) => {
+                            // Successfully converted to u32
+                            // println!("red {}", result);
+                            game.red = std::cmp::max(game.red, result);
+                        }
+                        Err(_) => {
+                            // Conversion failed
+                            println!("Failed to convert to u32");
+                        }
+                    }
+                }
+                if let Some(index) = part.find("blue") {
+                    let st = &part[index - 3..index];
+                    let mst = &st.replace(" ", "");
+
+                    match mst.parse::<u32>() {
+                        Ok(result) => {
+                            // Successfully converted to u32
+                            // println!("blue {}", result);
+                            game.blue = std::cmp::max(game.blue, result);
+                        }
+                        Err(_) => {
+                            // Conversion failed
+                            println!("Failed to convert to u32");
+                        }
+                    }
+                }
+                if let Some(index) = part.find("green") {
+                    let st = &part[index - 3..index];
+                    let mst = &st.replace(" ", "");
+
+                    match mst.parse::<u32>() {
+                        Ok(result) => {
+                            // Successfully converted to u32
+                            // println!("green {}", result);
+                            game.green = std::cmp::max(game.green, result);
+                        }
+                        Err(_) => {
+                            // Conversion failed
+                            println!("Failed to convert to u32");
+                        }
+                    }
+                }
+            });
+        } else {
+            println!("No colon found in the string");
+        }
+        println!("{} {} {}", game.red, game.blue, game.green);
+        if game.red <= 12 && game.green <= 13 && game.blue <= 14 {
+            sum += game.id;
+        }
+        game.red = 0;
+        game.blue = 0;
+        game.green = 0;
+        game.id += 1;
+    }
+    println!("sum = {}", sum);
+    Ok(())
+}
